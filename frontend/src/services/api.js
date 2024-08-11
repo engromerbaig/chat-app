3a4d4a6f7910b70d1a16cee1a18d3a24581a1ce9
Omer <engromerbaig@gmail.com>
2024-08-11 04:43:32 +0200

    npm installed axios in frontend and  created api.js in frontend directory for future intgeration

---
+++ frontend/src/services/api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+
+API.interceptors.request.use((req) => {
+    if (localStorage.getItem('token')) {
+        req.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+    }
+    return req;
+});
+
+export const login = (formData) => API.post('/auth/login', formData);
+export const register = (formData) => API.post('/auth/register', formData);
+export const sendMessage = (data) => API.post('/chats', data);
+export const getChats = (userId) => API.get(`/chats/${userId}`);
